eb24ba8ae0f0581c6a3a436e6727e23c44dba61c
KamilaCoder <kamila.spodymek@gmail.com>
2019-06-11 10:46:57 +0200

    Added a global app state in the index.js.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,36 @@
 import Search from './models/Search';
 
+/**Global state of the app 
+ * -Search object
+ * -Current recipe object
+ * -Shopping list object
+ * -Liked recipes
+ */
 
-const search = new Search('pizza');
-search.getResults();
+ const state = {};
+
+ const controlSearch = async () => {
+     // get the query from the view
+     const query = 'pizza';
+
+     if (query) {
+         // New serach object and add to state
+         state.search = new Search(query);
+         // Prepare UI for results
+
+         // Search for recipes
+         await state.search.getResults();
+
+         // Render results on UI
+         console.log(state.search.result)
+     }
+
+ }
+
+ document.querySelector('.search').addEventListener('submit', e => {
+     e.preventDefault();
+     controlSearch();
+
+ })
+
    
