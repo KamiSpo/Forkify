40de7ec2d303793f84acd89bff75992247eda7c6
KamilaCoder <kamila.spodymek@gmail.com>
2019-06-15 18:39:38 +0200

    created pagination

--- src/js/index.js
+++ src/js/index.js
@@ -39,4 +39,13 @@ import {elements, renderLoader, clearLoader} from './views/base';
 
  })
 
+ elements.searchResPages.addEventListener('click', e => {
+     const btn = e.target.closest('.btn-inline');
+     if (btn) {
+         const goToPage = parseInt(btn.dataset.goto, 10);
+         searchView.clearResults();
+         searchView.renderResults(state.search.result, goToPage)
+     }
+ })
+
    
