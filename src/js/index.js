68cc31d26caca3ceef5677c9aceb068472a5ba22
KamilaCoder <kamila.spodymek@gmail.com>
2019-06-16 21:10:59 +0200

    Created recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /**Global state of the app 
@@ -67,7 +68,9 @@ import {elements, renderLoader, clearLoader} from './views/base';
      const id = window.location.hash.replace('#', '');
 
      if (id) {
-         // Prepare UI for changes 
+         // Prepare UI for changes
+         recipeView.clearRecipe(); 
+         renderLoader(elements.recipe);
 
          // Create a new recipe object
         state.recipe = new Recipe(id);
@@ -82,7 +85,9 @@ import {elements, renderLoader, clearLoader} from './views/base';
             state.recipe.calcTime();
 
             // Render the recipe
-            console.log(state.recipe)
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (error) {
             alert('Error processing recipe!')
         }
