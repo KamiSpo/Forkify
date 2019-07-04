63d38bec1782e2e2083a93c4e36eabc7a12f80d9
KamilaCoder <kamila.spodymek@gmail.com>
2019-07-04 09:28:08 +0200

    Added storing likes in the local storage.

--- src/js/index.js
+++ src/js/index.js
@@ -147,10 +147,6 @@ import {elements, renderLoader, clearLoader} from './views/base';
   * LIKES CONTROLLER
   */
 
-//testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -182,6 +178,21 @@ const controlLike = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes()); 
 }
+
+ // Restore liked recipes on page load
+
+ window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like))
+ });
 
 
 // handling servings button clicks
